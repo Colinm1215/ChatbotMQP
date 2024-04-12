cae853c703283828c5471d442e83ed0295e1460f
Colinm1215 <colinm1215@gmail.com>
2024-04-12 18:31:31 -0400

    two camera test

--- realtime_testing.py
+++ realtime_testing.py
@@ -7,7 +7,7 @@ import pandas as pd
 import threading
 import queue
 
-#import config
+# import config
 
 warnings.filterwarnings('ignore')
 
@@ -21,6 +21,8 @@ class FaceRecognizer(threading.Thread):
         self.face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
         self.last_recognized_face = None
         self.search = True
+        self.cap_left = cv2.VideoCapture(0, cv2.CAP_ANY)
+        self.cap_right = cv2.VideoCapture(1, cv2.CAP_ANY)
 
     def get_known_faces(self):
         """Load known faces and labels from a folder."""
@@ -43,34 +45,42 @@ class FaceRecognizer(threading.Thread):
                 return self.labels[i]
         return "Unknown"
 
-    def calculate_turn(self, center_x_of_face, center_x_of_frame):
-        steps_per_degree = 10 # placeholder - set in config.py
-        HFOV = 90 # placeholder fov - should also be set in config.py
-        angle = ((center_x_of_face - center_x_of_frame) / center_x_of_frame) * (HFOV / 2)
+    def calculate_turn(self, center_x_left_camera, center_x_right_camera):
+        steps_per_degree = 10  # placeholder - set in config.py
+        HFOV = 90  # placeholder - should also be set in config.py
+        interocular_distance = 130  # placeholder - should also be set in config.py
+        center_x_avg = (center_x_left_camera + center_x_right_camera) / 2
+
+        angle = ((center_x_avg - center_x_left_camera) / interocular_distance) * (HFOV / 2)
+
         return angle * steps_per_degree
 
     def run(self):
         last_check_time = time.time()
-        cap = cv2.VideoCapture(0, cv2.CAP_ANY)
         cached_results = None
-        center_line_x = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH)) / 2
+        center_x_left = int(self.cap_left.get(cv2.CAP_PROP_FRAME_WIDTH)) / 2
+        center_x_right = int(self.cap_right.get(cv2.CAP_PROP_FRAME_WIDTH)) / 2
 
         while self.search:
-            ret, frame = cap.read()
-            if not ret:
+            ret_left, frame_left = self.cap_left.read()
+            ret_right, frame_right = self.cap_right.read()
+            if not ret_left or not ret_right:
                 self.message_queue.put("Error: Video capture failed")
                 break
 
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            faces = self.face_cascade.detectMultiScale(gray, 1.1, 4)
+            gray_left = cv2.cvtColor(frame_left, cv2.COLOR_BGR2GRAY)
+            gray_right = cv2.cvtColor(frame_right, cv2.COLOR_BGR2GRAY)
 
-            if len(faces) != 0:
+            faces_left = self.face_cascade.detectMultiScale(gray_left, 1.1, 4)
+            faces_right = self.face_cascade.detectMultiScale(gray_right, 1.1, 4)
+
+            if len(faces_left) != 0:
                 try:
                     db_path = os.path.abspath(self.images_folder_path)
                     cur_time = time.time()
-                    if cached_results is None or cur_time - last_check_time >= 10: # config.face_check_delay
+                    if cached_results is None or cur_time - last_check_time >= 10:  # config.face_check_delay
                         last_check_time = time.time()
-                        results = DeepFace.find(frame,
+                        results = DeepFace.find(frame_left,
                                                 db_path=db_path,
                                                 model_name="VGG-Face",
                                                 enforce_detection=False,
@@ -78,38 +88,61 @@ class FaceRecognizer(threading.Thread):
                         cached_results = results
 
                     if cached_results:
-                        for (x, y, w, h) in faces:
-                            cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)
+                        for (x, y, w, h) in faces_left:
+                            cv2.rectangle(frame_left, (x, y), (x + w, y + h), (255, 0, 0), 2)
 
-                            center_x_of_face = x + (h / 2)
-
-                            for result_df in cached_results:
-                                if isinstance(result_df, pd.DataFrame) and not result_df.empty:
-                                    for index, row in result_df.iterrows():
-                                        identity = row['identity']
-                                        recognized_face = os.path.basename(identity)
-                                        if recognized_face != self.last_recognized_face:
-                                            steps = self.calculate_turn(center_x_of_face, center_line_x)
-                                            direction = "none"
-                                            if steps > 0:
-                                                direction = "right"
-                                            elif steps < 0:
-                                                direction = "left"
-                                            steps = abs(steps)
-                                            self.message_queue.put(f"{recognized_face.split('.', 1)[0]},{steps},{direction}")
-                                            self.last_recognized_face = recognized_face
-                                            break
-
+                            center_x_left = x + (h / 2)
                 except Exception as e:
                     continue
-                    #self.message_queue.put(f"Error: {e}")
+                    # self.message_queue.put(f"Error: {e}")
 
-            cv2.imshow('Webcam', frame)
+            if len(faces_right) != 0:
+                try:
+                    db_path = os.path.abspath(self.images_folder_path)
+                    cur_time = time.time()
+                    if cached_results is None or cur_time - last_check_time >= 10:  # config.face_check_delay
+                        last_check_time = time.time()
+                        results = DeepFace.find(frame_left,
+                                                db_path=db_path,
+                                                model_name="VGG-Face",
+                                                enforce_detection=False,
+                                                silent=True)
+                        cached_results = results
+
+                    if cached_results:
+                        for (x, y, w, h) in faces_right:
+                            cv2.rectangle(frame_right, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+                            center_x_right = x + (h / 2)
+                except Exception as e:
+                    continue
+                    # self.message_queue.put(f"Error: {e}")
+
+            for result_df in cached_results:
+                if isinstance(result_df, pd.DataFrame) and not result_df.empty:
+                    for index, row in result_df.iterrows():
+                        identity = row['identity']
+                        recognized_face = os.path.basename(identity)
+                        if recognized_face != self.last_recognized_face:
+                            steps = self.calculate_turn(center_x_right, center_x_left)
+                            direction = "none"
+                            if steps > 0:
+                                direction = "right"
+                            elif steps < 0:
+                                direction = "left"
+                            steps = abs(steps)
+                            self.message_queue.put(f"{recognized_face.split('.', 1)[0]},{steps},{direction}")
+                            self.last_recognized_face = recognized_face
+                            break
+
+            cv2.imshow('Left', frame_left)
+            cv2.imshow('Right', frame_right)
 
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 self.search = False
 
-        cap.release()
+        self.cap_left.release()
+        self.cap_right.release()
         cv2.destroyAllWindows()
 
 
